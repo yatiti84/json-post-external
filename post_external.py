c7d3c62e4172a7a460833bbe4bacf062464a83a3
su <yatiti84@gmail.com>
2022-01-06 17:13:09 +0800

    fix(py file): define column, structure renew

--- post_external.py
+++ post_external.py
@@ -1,21 +1,12 @@
 import requests
 import json
 from google.cloud import storage
-def get_columnNames():
-    url = "https://www.mirrormedia.mg/json/grouped.json"
-    file = requests.get(url)
-    data = json.loads(file.text)
-    column_name = list(data['grouped'][0].keys())
-    column_name.append('publishedDate')
-    return column_name
-def get_data(url):
+
+column_names =["slug","title", "style", "sections", "categories", "heroImage", "brief", "publishedDate"]
+def get_results(url, api_endpoint, results):
     file = requests.get(url)
     data = json.loads(file.text)
     data = data['_items']
-    return data
-def get_post_results(data):
-    column_names = get_columnNames()
-    results = []
     for item in data:
         result = {}
         for columnName in column_names:
@@ -23,45 +14,33 @@ def get_post_results(data):
                 result[columnName] = item[columnName]
             else:
                 result[columnName] = ""
+        if api_endpoint == "externals":
+            result['slug'] = item['_id'] 
+            result['heroImage'] = item['thumb'] 
         results.append(result)
-    return results
-def get_external_results(data):
-    column_names = get_columnNames()
+    return results   
+def homepage_json():
     results = []
-    for item in data:
-        result = {}
-        for columnName in column_names:
-            if(columnName in item):
-                result[columnName] = item[columnName]
-            else:
-                result[columnName] = ""
-        result['slug'] = item['_id'] 
-        result['heroImage'] = item['thumb'] 
-        results.append(result)
-    return results
-def join_post_external(results1, result2):
-    results = results1
-    for i in result2:
-        results.append(i)
-    return results
+    api_endpoints= ["posts", "externals"]
+    mm_url = "https://api.mirrormedia.mg/"
+    post_parameter = "?sort=-updateAt&where=%7B%22isAdvertised%22:false,%22state%22:%7B%22$ne%22:%22invisible%22%7D,%22categories%22:%7B%22$nin%22:%5B%22581c3a7792c2930d009de311%22,%225ea94861a66f9e0f00a0503f%22%5D%7D%7D&max_results=100&page="
+    external_parameter = "?max_results=100&sort=-publishedDate&page="
+    results = []
+    for api_endpoint in api_endpoints:
+        if api_endpoint == "posts":
+            parameter = post_parameter
+        else:
+            parameter = external_parameter
 
-def homepage_json() :
-    column_names = get_columnNames()
-    url_post_p1 = "https://api.mirrormedia.mg/posts?sort=-updateAt&where=%7B%22isAdvertised%22:false,%22state%22:%7B%22$ne%22:%22invisible%22%7D,%22categories%22:%7B%22$nin%22:%5B%22581c3a7792c2930d009de311%22,%225ea94861a66f9e0f00a0503f%22%5D%7D%7D&max_results=100&page=1"
-    url_post_p2 = "https://api.mirrormedia.mg/posts?sort=-updateAt&where=%7B%22isAdvertised%22:false,%22state%22:%7B%22$ne%22:%22invisible%22%7D,%22categories%22:%7B%22$nin%22:%5B%22581c3a7792c2930d009de311%22,%225ea94861a66f9e0f00a0503f%22%5D%7D%7D&max_results=100&page=2"
-    post_p1 = get_post_results(get_data(url_post_p1))
-    post_p2 = get_post_results(get_data(url_post_p2))
+        for i in range(1, 3):
+            url = mm_url + api_endpoint +parameter + str(i)
+            get_results(url, api_endpoint, results)
 
-    url_external_p1 = "https://api.mirrormedia.mg/externals?max_results=100&sort=-publishedDate&page=1"
-    url_external_p2 = "https://api.mirrormedia.mg/externals?max_results=100&sort=-publishedDate&page=2"
-    extetnal_p1 = get_external_results(get_data(url_external_p1))
-    extetnal_p2 = get_external_results(get_data(url_external_p2))
-
-    post_external = join_post_external(join_post_external(post_p1, post_p2), join_post_external(extetnal_p1, extetnal_p2))
-    post_external = sorted(post_external, key=lambda k:k['publishedDate'])
-    json_file = {'_items': post_external}
+    results = sorted(results, key=lambda k:k['publishedDate'])
+    post_external = {'_items': results}
     with open('post_external.json', 'w') as f:
-        json.dump(json_file, f)
+        json.dump(post_external, f)   
+
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
     storage_client = storage.Client()
